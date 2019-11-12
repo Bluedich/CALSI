47bd020c279d97faa5b44991956d55529b1c701d
erwanrenon <renon.erwan@outlook.com>
2019-11-12 12:03:28 +0100

    premier jet interface vitrine

--- src/main/java/org/frontend/FXMLController.java
+++ src/main/java/org/frontend/FXMLController.java
@@ -1,14 +1,43 @@
 package org.frontend;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
 
 public class FXMLController {
 
     @FXML
-    private Label label;
-
+    public Label rightStatus;
+    public Button startButton;
+    
+    ObservableList<String> content = FXCollections.observableArrayList(
+    		"e = 3", "d = 7");
+    ObservableList<String> content2 = FXCollections.observableArrayList(
+    		"a = 1", "b = 2" , "c = 3", "d = 4");
+    
+    public ListView<String> listView = new ListView<String>(content);
+    public ListView<String> listView2 = new ListView<String>(content2);
+   
+    public ChoiceBox<String> choiceBox = new ChoiceBox<String>();
+    public ChoiceBox<String> choiceBox2 = new ChoiceBox<String>();
+    
     public void initialize() {
-        label.setText("Hello, JavaFX");
+    	choiceBox.getItems().addAll("P1", "P2", "P3", "P4");
+    	choiceBox.setValue("P4");
+    	choiceBox2.getItems().addAll("Step-by-step", "Random" , "With File");
+    	choiceBox2.setValue("Step-by-step");
+    	listView.setItems(content);
+    	listView2.setItems(content2);
+    	
     }
+
+    public void onStartClicked(){
+    	rightStatus.setText("Running...");
+    }
+        
 }
