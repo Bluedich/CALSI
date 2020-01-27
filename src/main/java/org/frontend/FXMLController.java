c69d54c6e437c98a161547320429c664adbd6c28
kloupa <pollet.cyril54@gmail.com>
2020-01-27 16:50:06 +0100

    add example

--- src/main/java/org/frontend/FXMLController.java
+++ src/main/java/org/frontend/FXMLController.java
@@ -140,7 +140,29 @@ public class FXMLController {
 
 	boolean auto = false;
 	private static DecimalFormat df = new DecimalFormat("0.0");
-	private String code="Ici votre code";
+	private String code="import java.lang.Math; 	//	All import have to be made without any blank line\r\n" + 
+			"\r\n" + 
+			"// // Shared variables declaration		//	All declaration have to be made without any blank line\r\n" + 
+			"Integer shared1;\r\n" + 
+			"Boolean[] shared2;\r\n" + 
+			"\r\n" + 
+			"// // Shared variables initialization		//	All initialization have to be made without any blank line\r\n" + 
+			"flag[0] = false;\r\n" + 
+			"flag[1] = false;\r\n" + 
+			"\r\n" + 
+			"// // Local variables declaration		//	All declaration have to be made without any blank line\r\n" + 
+			"int j;\r\n" + 
+			"boolean a;\r\n" + 
+			"\r\n" + 
+			"// //Example algorithm		//	You can write your algorithm using Java Syntax, blank line are allowed\r\n" + 
+			"flag[i] = true;\r\n" + 
+			"while ( a == true && b == j) {		//	The opening accolade \"{\" have to be in the same line than the condition\r\n" + 
+			"	toto=test\r\n" + 
+			"}		//	The closing accolade \"}\" have to be alone on the line\r\n" + 
+			"// critical section \r\n" + 
+			"1\r\n" + 
+			"// end of critical section\r\n" + 
+			"flag[i] = false;";
 	private String fichiercode="";
 	private String cordo="";
 	private int numberOfProcesses;
